ee17a7ca7bfe0ec945ec6525f8ff763533885a7a
No-2004 <noamhendriks*@gmail.com>
2025-05-07 16:00:05 +0200

    main.py functional and wing.py initiated.

--- concept_analysis/main.py
+++ concept_analysis/main.py
@@ -3,7 +3,7 @@ import numpy as np
 #Mission parameters:
 m_payload = 400 #kg
 range = 120 #km
-landings = 3
+landings = 2
 v_cruise = 200/3.6 #m/s
 v_hover = 67/3.6 #m/s
 w_hover = 6 #m
@@ -18,7 +18,8 @@ t_hover = landings*t_landing #sec
 t_cruise = range * 1000 / v_cruise #sec
 
 LD_ratio = 10 #Lift to drag ratio
-
+eff_motor = 0.95 #Efficiency of the motor
+eff_propeller = 0.85 #Efficiency of the propeller
 
 N_disks = 6 #Number of disks (between 4 and 8)
 D_rotor = 3 #m (max w_hover/2)
@@ -30,30 +31,38 @@ C_D = 0.4 #Drag coefficient of the front of the vehicle
 #Constants:
 g = 9.81 #m/s^2
 rho_air = 1.225 #kg/m^3
-density_batt = 250*3600*0.8 #Wh/kg, 80% depth of discharge
+density_batt = 250*3600*0.8*0.85 #Wh/kg, 80% depth of discharge
 
 #Code parameters:
 battery = True
-wing = False
+wing = True
 mtow = 2500 * g #N
 mtow_prev = 0 #N
+n = 1
 
-while abs(mtow_prev - mtow) > 0.1:
-    print('Hey, I am in the loop')
+print("Winged: ", wing)
+
+if not wing:
+    S_disks = 120 #m^2 (for now, but can be changed later)
+diameter_rotor = 2 * np.sqrt(S_disks/N_disks / np.pi) #m (diameter of the rotor)
+print("Diameter of the rotor: ", diameter_rotor, "m")
+
+while abs(mtow_prev - mtow) > 0.1 and n<1000:
+    n+=1
     #Energy calculation:
     if wing:
         T = mtow / LD_ratio #N
         P_cruise = T * v_cruise #W
     else:
         F_sideways = 0.5 * rho_air * (v_cruise**2) * A_front * C_D #N
-        T = np.sqrt(mtow + F_sideways)
+        T = np.sqrt(mtow**2 + F_sideways**2)
         P_cruise = 1.15 * T**(3/2) / np.sqrt(2 * rho_air * S_disks) #W
     E_cruise = P_cruise * t_cruise #J
 
     T = mtow #N
     P_induced = 1.15 * T**(3/2) / np.sqrt(2 * rho_air * S_disks) #W
     P_profile = 0 # Neglect for now, but can be added later
-    P_hover = P_induced + P_profile #W
+    P_hover = (P_induced + P_profile) / eff_motor / eff_propeller #W
     E_hover = P_hover * t_hover #J
 
     E_total = E_cruise + E_hover #J
@@ -63,12 +72,15 @@ while abs(mtow_prev - mtow) > 0.1:
 
     if wing:
         # Wing mass, area, drag as a function of mtow, v_cruise
-        m_eom = 2200 / 3900 * mtow #kg (ratio for winged vehicles from https://www.researchgate.net/publication/318235979_A_Study_in_Reducing_the_Cost_of_Vertical_Flight_with_Electric_Propulsion/figures)
+        m_eom = 2200/3900 * mtow/g #kg (ratio for winged vehicles from https://www.researchgate.net/publication/318235979_A_Study_in_Reducing_the_Cost_of_Vertical_Flight_with_Electric_Propulsion/figures)
     else:
-        m_eom = 1700/3450 * mtow #kg (ratio for non-winged vehicles from https://www.researchgate.net/publication/318235979_A_Study_in_Reducing_the_Cost_of_Vertical_Flight_with_Electric_Propulsion/figures)
+        m_eom = 1500/3450 * mtow/g #kg (ratio for non-winged vehicles from https://www.researchgate.net/publication/318235979_A_Study_in_Reducing_the_Cost_of_Vertical_Flight_with_Electric_Propulsion/figures)
 
     mtow_prev = mtow #N
-    mtow = m_payload + m_powersource + m_eom #N
+    mtow = (m_payload + m_powersource + m_eom) * g #N
 
+print("Number of iterations: ", n)
 print("Final MTOW: ", mtow / g, "kg")
-print("Final Power source ratio: ", m_powersource/mtow*100, "%")
+print("Final Power source ratio: ", m_powersource*g/mtow*100, "%")
+print("Final eom mass ratio: ", m_eom*g/mtow*100, "%")
+print(P_hover/1000)
--- concept_analysis/wing.py
+++ concept_analysis/wing.py
@@ -1,1 +1,22 @@
 # Wing mass, area, drag as a function of mtow, v_cruise
+class Wing:
+    def __init__(self, mtow, v_cruise, S_disks, rho_air, g):
+        self.mtow = mtow
+        self.v_cruise = v_cruise
+        self.S_disks = S_disks
+        self.rho_air = rho_air
+        self.g = g
+
+    def calculate_wing_mass(self):
+        # Constants for wing mass estimation (these values are placeholders and should be replaced with actual data)
+        wing_area = 20  # m^2 (example value for wing area)
+        wing_loading = self.mtow / wing_area  # N/m^2 (wing loading)
+        wing_mass = 0.1 * wing_area * (wing_loading ** 0.5)  # kg (example formula for wing mass estimation)
+        return wing_mass
+
+    def calculate_drag_coefficient(self):
+        # Constants for drag coefficient estimation (these values are placeholders and should be replaced with actual data)
+        aspect_ratio = 10  # example value for aspect ratio
+        C_D0 = 0.02  # zero-lift drag coefficient
+        C_D = C_D0 + (1 / aspect_ratio) * (self.mtow / self.v_cruise) ** 2  # drag coefficient estimation
+        return C_D
