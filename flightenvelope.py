3d6f536d2fe90ec4bd9a6927a0b3e0e4c77734df
gaiaejlal <119508297+gaiaejlal@users.noreply.github.com>
2025-06-03 16:47:59 +0200

    more

--- flightenvelope.py
+++ flightenvelope.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 W = 22000
 clmax = 1.7
 S = 14
+clalpha = 2*np.pi #per radian
 
 n_max = 2.5 
 n_min = -1.0
 Vc = 200 / 3.6 
 rho = 1.225 
+Ude = 50 * 0.3048 #gust velicity in m/s
 
 Vs = np.sqrt(W / (0.5 * rho * S * clmax))
 Va = Vs * np.sqrt(n_max)
@@ -18,8 +20,7 @@ Vneg = Vs * np.sqrt(abs(n_min))
 print(f"Vs: {Vs:.2f} m/s, Va: {Va:.2f} m/s, Vd: {Vd:.2f} m/s, Vneg: {Vneg:.2f} m/s")
 
 
-
-# === Create velocity array for plotting ===
+# Create velocity array for plotting 
 V = np.linspace(0, Vd, 500)
 n_stall_pos = (V / Vs)**2
 n_stall_neg = -(V / Vs)**2
@@ -28,27 +29,36 @@ n_stall_neg = -(V / Vs)**2
 n_stall_pos = np.clip(n_stall_pos, 0, n_max)
 n_stall_neg = np.clip(n_stall_neg, n_min, 0)
 
-# === Plotting ===
+#for gust load
+ws = W / S
+delta_n = (rho * V * Ude * clalpha) / (2 * ws)
+
+# Total gust load factors
+n_gust_pos = 1 + delta_n
+n_gust_neg = 1 - delta_n
+
+# Plotting 
 plt.figure(figsize=(10, 6))
-plt.plot(V, n_stall_pos, label='Positive Stall Limit Curve', color='green')
-plt.plot(V, n_stall_neg, label='Negative Stall Limit Curve', color='purple')
+plt.plot(V, n_stall_pos, label='Positive Stall Limit Curve', color='black')
+plt.plot(V, n_stall_neg, label='Negative Stall Limit Curve', color='black')
+
+# Horizontal lines for load limits
+plt.axhline(y=n_max, color='black', linestyle='--')
+plt.axhline(y=n_min, color='black', linestyle='--')
+plt.plot([Va, Vd], [n_max, n_max], color='black', label='n_max')
+plt.plot([Vneg, Vd], [n_min, n_min], color='black',  label='n_min')
+plt.axhline(y=0, color='lightgray', linestyle=':')
 
 # Vertical lines for key speeds
 plt.axvline(x=Vs, color='blue', linestyle='--', label='Vs')
-plt.axvline(x=Va, color='cyan', linestyle='--', label='Va')
-plt.axvline(x=Vneg, color='magenta', linestyle='--', label='Vneg')
-plt.axvline(x=Vc, color='orange', linestyle='--', label='Vc')
-plt.plot([Vd, Vd], [n_min, n_max], color='lightcoral', linestyle='--')
-plt.axvline(x=Vd, color='lightcoral', label='Vd')
+plt.axvline(x=Va, color='blue', linestyle='--', label='Va')
+plt.axvline(x=Vc, color='blue', linestyle='--', label='Vc')
+plt.axvline(x=Vd, color='blue', label='Vd', linestyle='--')
+plt.plot([Vd, Vd], [n_min, n_max], color='black')
 
+plt.plot(V, n_gust_pos, 'orange', linestyle='--', label='Gust Upper Limit')
+plt.plot(V, n_gust_neg, 'orange', linestyle='--', label='Gust Lower Limit')
 
-# Horizontal lines for load limits
-
-plt.axhline(y=n_max, color='blue', linestyle='--')
-plt.axhline(y=n_min, color='salmon', linestyle='--')
-plt.axhline(y=0, color='gray', linestyle='-')
-plt.plot([Va, Vd], [n_max, n_max], color='blue', linestyle='-', label='n_max')
-plt.plot([Vneg, Vd], [n_min, n_min], color='salmon', linestyle='-', label='n_min')
 
 
 # Plot styling
